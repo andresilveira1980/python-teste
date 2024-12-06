da5059365da453749b333d4c3814ef765d371286
andresilveira1980 <andrehbahia@gmail.com>
2024-12-06 20:00:28 -0300

    contaPares

--- estrutura-repeticao.py
+++ estrutura-repeticao.py
@@ -30,3 +30,16 @@ while True:
 
 for nome in nomes:
     print(nome)
+
+print('-----------------------Programa que conta pares ------------------------')
+print()
+
+contaPares = 0
+i = 1
+for i in range(5):
+    i+=1
+    valor = int(input('Digite um número: '))
+    if valor%2 == 0:
+        contaPares += 1
+print('Você digitou ', contaPares, 'números pares')    
+        
